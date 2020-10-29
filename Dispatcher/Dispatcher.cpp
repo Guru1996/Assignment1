c6936672ec7cc04613e0685dc50afd47fe9d8b2c
Guru1996 <shergill000916@gmail.com>
2020-10-29 02:49:32 -0700

    Add files via upload

---
+++ Dispatcher/Dispatcher.cpp
@@ -0,0 +1,23 @@
+#include "C:\RTExamples\RTExamples\rt.h"
+
+
+int Elevator1_status_dealer(void* ThreadArgs) {
+	
+	return 0;
+}
+
+int Elevator2_status_dealer(void* ThreadArgs) {
+
+	return 0;
+}
+
+int Thread3(void* ThreadArgs) {
+
+	return 0;
+}
+
+int main(void) {
+
+
+	return 0;
+}
