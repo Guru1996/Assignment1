ee6e560bfce110f98f93293ad0e7527d1b4e327a
Anikesh Gupta <98anikesh@gmail.com>
2020-11-04 03:56:00 +0530

    Dispatcher draft algo ready

--- Dispatcher/Dispatcher.cpp
+++ Dispatcher/Dispatcher.cpp
@@ -104,6 +104,7 @@ int main(void) {
 			else {
 				elevator2.Post(111); //send fault resolved to e2
 			}
+
 		//terminate command to elevators
 		}
 		else if (command[0] == 'e' && command[1] == 'e') {
@@ -111,16 +112,72 @@ int main(void) {
 			elevator2.Post(555);//stop accepting commands
 		}
 
+		//incase both elevators at fault
+		else if (e1_data.Generalstatus == 1 && e2_data.Generalstatus == 1){
+		//do nothing
+		}
+
 		//Up and down general commands  -----PENDING----MAIN FUNCTIONALITY----==================
 		else if (command[0] == 'u' || command[0] == 'd') {
+			//CATEGORY 0: one of them not working
 			if (e1_data.Generalstatus == 0 && e2_data.Generalstatus == 1) {
-				elevator2.Post(100 + (int)(command[1]) - (int)('0')); //send command to elevator 2
+				elevator2.Post(100 + (int)(command[1]) - (int)('0')); //send command to elevator 2 if 1 not working
 			}
 			else if (e2_data.Generalstatus == 0 && e1_data.Generalstatus == 1) {
-				elevator1.Post(100 + (int)(command[1]) - (int)('0')); //send command to elevator 1
+				elevator1.Post(100 + (int)(command[1]) - (int)('0')); //send command to elevator 1 if 2 not working
 			}
 
+			//CATERGORY 1: same direction requests
+			//up request and lift 1 going up and floor requested higher than current floor
+			else if (command[0] == 'u' && e1_data.direction== 1 && ( (((int)(command[1]) - (int)('0')) - e1_data.floor) > 0)) {
+				elevator1.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			//up request and lift 2 going up and floor requested higher than current floor
+			else if (command[0] == 'u' && e2_data.direction == 1 && ( (((int)(command[1]) - (int)('0')) - e2_data.floor) > 0)) {
+				elevator2.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			//down request and lift 1 going down and floor requested lower than current floor
+			else if (command[0] == 'd' && e1_data.direction == 2 && ((e1_data.floor - ((int)(command[1]) - (int)'0')) > 0)) {
+				elevator1.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			//down request and lift 2 going down and floor requested lower than current floor
+			else if (command[0] == 'd' && e2_data.direction == 2 && ((e2_data.floor- ((int)(command[1]) - (int)('0'))) > 0)) {
+				elevator2.Post(100 + (int)(command[1]) - (int)('0'));
+			}
 
+			//CATEGORY 2: stationary elevators
+			// both elevators stationary, give preference to lesser floor difference
+			else if (e2_data.direction == 0 && e1_data.direction == 0)
+				{
+				if (abs(e2_data.floor - ((int)(command[1]) - (int)('0'))) < abs(e1_data.floor - ((int)(command[1]) - (int)('0')))) {
+					elevator2.Post(100 + (int)(command[1]) - (int)('0'));
+					}
+				else {
+					elevator1.Post(100 + (int)(command[1]) - (int)('0'));
+				}
+				}
+			//elevator 2 is stationary, send that
+			else if (e2_data.direction == 0) {
+				elevator2.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			//elevator 1 is stationary, send that
+			else if (e1_data.direction == 0) {
+				elevator2.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			
+			//CATEGORY 3: elevators moving in unfavourable direction, send command to the one farther from it 
+			else if (abs(e2_data.floor - ((int)(command[1]) - (int)('0'))) > abs(e1_data.floor - ((int)(command[1]) - (int)('0')))) {
+				elevator2.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			else if (abs(e2_data.floor - ((int)(command[1]) - (int)('0'))) < abs(e1_data.floor - ((int)(command[1]) - (int)('0')))) {
+				elevator1.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			
+			//CATEGORY 4: incase a command doesn't satisty any of the conditions above
+			else {
+				elevator1.Post(100 + (int)(command[1]) - (int)('0'));
+			}
+			
 		}
 
 		//
