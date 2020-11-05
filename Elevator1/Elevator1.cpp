a28e7714e7edac84c1ad633d0b4cb765b3fcea1a
Anikesh Gupta <98anikesh@gmail.com>
2020-11-05 21:40:49 +0530

    Elevator code cleaned up

--- Elevator1/Elevator1.cpp
+++ Elevator1/Elevator1.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 using namespace std;
 
-CRendezvous     s1("Start", 4);
+CRendezvous s1("Start", 4);
 Monitor e1("elevator_1");
 monitorData e1_data;
 int floors_to_stop[10] = { 0 };
 
 UINT command;
+UINT local_command = 0;
 CMailbox   MyMailBox;
-
 CMutex* command1_mutex = new CMutex("command1_mutex");
-
+CMutex* local_command_mutex = new CMutex("local_command_mutex");
 //update_status(int status, int floor, int direction, int door)
 
 UINT __stdcall Get_commands(void* ThreadArgs) {
-
+	//999 means fault +1,  111 means resolved -1 , 555 meanss termination go back to 0
 	while (1) {
 		command = MyMailBox.GetMessage();
 		if (command >= 100 && command <= 109) {
@@ -24,22 +24,37 @@ UINT __stdcall Get_commands(void* ThreadArgs) {
 			floors_to_stop[command % 100] = 1;
 			command1_mutex->Signal();
 		}
+		else {
+			local_command_mutex->Wait();
+			local_command = command;
+			local_command_mutex->Signal();
+		}
 	}
 	return 0;
 }
 
-int pending_floor(int current_floor, int direction)
+//return true if elevator has pending requests in the current input direction
+int pending_request(int current_floor, int direction)
 {
+	int dummy;
 	if (direction == 1) {
 		for (int a = current_floor; a++; a < 9) {
-			if (floors_to_stop[a]) {
+			//protecting floor_to_stop using mutex
+			command1_mutex->Wait();
+			dummy = floors_to_stop[a];
+			command1_mutex->Signal();
+			if (dummy) {
 				return 1;
 			}
 		}
 	}
 	else if (direction == 2) {
 		for (int a = current_floor; a--; a > 0) {
-			if (floors_to_stop[a]) {
+			//protecting floor_to_stop using mutex
+			command1_mutex->Wait();
+			dummy = floors_to_stop[a];
+			command1_mutex->Signal();
+			if (dummy) {
 				return 1;
 			}
 		}
@@ -49,106 +64,137 @@ int pending_floor(int current_floor, int direction)
 
 }
 
+//gives dirrection where requests are pending, else returns 0 = stationary
+int get_direction(int current_floor) {
+
+	if (pending_request(current_floor, 1)) { //this will only return true if elevator has UP request
+		return 1;
+	}
+	else if (pending_request(current_floor, 2)) { //this will return true if elevator has a down request
+		return 2;
+	}
+	else {
+		return 0;
+	}
+}
+
+
 int main(void) {
-	s1.Wait();
 	int current_floor = 0;
 	int status = 1;
 	int direction = 0;
 	int door = 1;
-	int tracker = 0;
-	int dirr;
-	//just keep moving in the same direction as long as there are any requests in that direction
-	while (1) { //999 means fault +1,  111 means resolved , 555 meanss termination go back to 0---pending
+	int dummy;
+	int dummy_command;
+	
+	//begin thread
+	CThread   t1(Get_commands, ACTIVE, NULL);
 
-		command1_mutex->Wait();
-		if (floors_to_stop[current_floor]) {
-			//stop and open the door, wait for it to close 
-			floors_to_stop[current_floor] = 0; //clear request
-			//direction = 0; //
-			//...
+	//sync up with other processes and threads
+	s1.Wait();
+
+	//keep moving in the same direction as long as there are any requests in that direction
+	while (1) {//999 means fault +1,  111 means resolved , 555 meanss termination go back to 0
+		local_command_mutex->Wait();
+		dummy_command = local_command;
+		local_command_mutex->Signal();
+		if (dummy_command == 555) {
+			while (current_floor > 0) {
+				Sleep(1000);
+				current_floor--;
+				e1.update_status(status, current_floor, direction, door); //updating current status
+			}
+			//open the door at 0 floor
+			if (current_floor == 0) {
+				Sleep(500); //wait for door to open
+				door = 1;
+				direction = 0;
+				e1.update_status(status, current_floor, direction, door); //updating current status
+			}
 		}
 
 		else {
-			if (direction = 1) {
-				Sleep(1000);
-				current_floor++;
-			}
-			else if (direction = 2) {
-				Sleep(1000);
-				current_floor--;
+			local_command_mutex->Wait();
+			dummy_command = local_command;
+			local_command_mutex->Signal();
+			if (dummy_command == 999) {
+				status = 0;
+				e1.update_status(status, current_floor, direction, door); //updating current status
+				//clear all pending requests
+				for (int i = 0; i < 10; i++) {
+					command1_mutex->Wait();
+					floors_to_stop[i] = 0; //clear request
+					command1_mutex->Signal();
+				}
+
 			}
 			else {
-				
-				//decide where to go?
-				if (pending_floor(current_floor, 1)) {
-					direction = 1;
-					//continue lift in that direction 
-				}
-
-				/*else if (pending_floor(current_floor, )) {
-					direction = 2;
-				}*/
-				else {
-					if (direction == 1) {
-						dirr = 2;
-					}
-					else {
-						dirr = 1;
-					}
-
-					if (pending_floor(current_floor, dirr)) {
-						direction = dirr;
-					}
-
-					else {
-						direction = 0;
-					}
-			}
-			//continue -- sleep + counter ++ , -- 
-		}
-
-		/*
-		if (direction == 1)
-		{
-
-			if (floors_to_stop[current_floor]) {
-				//stop and open the door, wait for it to close 
-				//...clear the floor from the array
-				//...
-
-				//check if any pending floors in the same direction
-				if (pending_floor(current_floor, 1)) {
-					direction = 1;
-					Sleep(2000);
-
-
-					
-				}
-				else if (pending_floor(current_floor, 2)) {
-					direction = 2;
-				}
-				else {
+				local_command_mutex->Wait();
+				dummy_command = local_command;
+				local_command_mutex->Signal();
+				if (dummy_command == 111) {
+					status = 1;
 					direction = 0;
+					door = 0;
+					e1.update_status(status, current_floor, direction, door); //updating current status
+				}
+			}
+		}
+
+		//main code
+		local_command_mutex->Wait();
+		dummy_command = local_command;
+		local_command_mutex->Signal();
+
+		if (status == 1 && dummy_command != 555) {
+			command1_mutex->Wait();
+			dummy = floors_to_stop[current_floor];
+			command1_mutex->Signal();
+			//stopping at requested stop //stop and open the door, wait for it to close 
+			if (dummy) {
+
+				door = 1; //open door
+				e1.update_status(status, current_floor, direction, door); //updating current status
+
+				//protecting the array with mutex //clear floor request
+				command1_mutex->Wait();
+				floors_to_stop[current_floor] = 0; 
+				command1_mutex->Signal();
+
+				//close the door and update the status
+				Sleep(500); //wait for door close
+				door = 2;
+				e1.update_status(status, current_floor, direction, door); //updating current status
+
+				//DECIDE where to go next: change direction if no more pending requests in the same direction
+				if (!pending_request(current_floor, direction)) {
+					direction = get_direction(current_floor);
+					e1.update_status(status, current_floor, direction, door); //updating current status
 				}
 			}
 
-		}
-		
-		//direction down
-		else if (direction==2)
-		{
+			//move the lift, change floors
+			if (direction == 0) {
+				direction = get_direction(current_floor);
+				e1.update_status(status, current_floor, direction, door); //updating current status
+			}
+
+			if (direction == 1) {
+				Sleep(1000);
+				current_floor++;
+				e1.update_status(status, current_floor, direction, door); //updating current status
+			}
+
+			if (direction == 2) {
+				Sleep(1000);
+				current_floor--;
+				e1.update_status(status, current_floor, direction, door); //updating current status
+			}
 
 		}
 
-		//direction stationary
-		else if (direction == 0) {
+	}//while loop end
 
-		}
-		*/
-		command1_mutex->Signal();
-	}
-
-
-
+	t1.WaitForThread();
 	return 0;
 }
--- elevator1_dummy.cpp
+++ elevator1_dummy.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 using namespace std;
 
-CRendezvous     s1("Start", 4);
+CRendezvous s1("Start", 4);
 Monitor e1("elevator_1");
 monitorData e1_data;
 int floors_to_stop[10] = { 0 };
 
 UINT command;
-UINT local_command=0;
+UINT local_command = 0;
 CMailbox   MyMailBox;
 CMutex* command1_mutex = new CMutex("command1_mutex");
 CMutex* local_command_mutex = new CMutex("local_command_mutex");
 //update_status(int status, int floor, int direction, int door)
 
 UINT __stdcall Get_commands(void* ThreadArgs) {
-	//999 means fault +1,  111 means resolved , 555 meanss termination go back to 0---pending
+	//999 means fault +1,  111 means resolved -1 , 555 meanss termination go back to 0
 	while (1) {
 		command = MyMailBox.GetMessage();
 		if (command >= 100 && command <= 109) {
@@ -32,28 +32,29 @@ UINT __stdcall Get_commands(void* ThreadArgs) {
 	}
 	return 0;
 }
-//return true if elevator has to move in the direction of current input direction
-int pending_floor(int current_floor, int direction)
+
+//return true if elevator has pending requests in the current input direction
+int pending_request(int current_floor, int direction)
 {
 	int dummy;
 	if (direction == 1) {
 		for (int a = current_floor; a++; a < 9) {
-
+			//protecting floor_to_stop using mutex
 			command1_mutex->Wait();
 			dummy = floors_to_stop[a];
 			command1_mutex->Signal();
-
-			if (dummy) {//protect it using mutex
+			if (dummy) {
 				return 1;
 			}
 		}
 	}
 	else if (direction == 2) {
 		for (int a = current_floor; a--; a > 0) {
+			//protecting floor_to_stop using mutex
 			command1_mutex->Wait();
 			dummy = floors_to_stop[a];
 			command1_mutex->Signal();
-			if (dummy) {//protect it using mutex
+			if (dummy) {
 				return 1;
 			}
 		}
@@ -63,49 +64,55 @@ int pending_floor(int current_floor, int direction)
 
 }
 
-int getting_direction(int current_floor) {
+//gives dirrection where requests are pending, else returns 0 = stationary
+int get_direction(int current_floor) {
 
-	//deciding the direction if current direction is zero i.e. elevator is stationary
-	if (pending_floor(current_floor, 1)) { //this will only return true if elevator has UP request
-		//// pending floor will return true if elevator has to move in the direction of current input direction which is one here
+	if (pending_request(current_floor, 1)) { //this will only return true if elevator has UP request
 		return 1;
 	}
-	else {
-		if (pending_floor(current_floor, 2)) { //this will return true if elevator has a down request
-			return 2;
-		}
-		else {
-			return 0;
-		}
+	else if (pending_request(current_floor, 2)) { //this will return true if elevator has a down request
+		return 2;
 	}
-
+	else {
+		return 0;
+	}
 }
+
+
 int main(void) {
-	s1.Wait();
 	int current_floor = 0;
 	int status = 1;
 	int direction = 0;
 	int door = 1;
 	int dummy;
 	int dummy_command;
+	
+	//begin thread
 	CThread   t1(Get_commands, ACTIVE, NULL);
-	//just keep moving in the same direction as long as there are any requests in that direction
-	while (1) {//999 means fault +1,  111 means resolved , 555 meanss termination go back to 0---pending
+
+	//sync up with other processes and threads
+	s1.Wait();
+
+	//keep moving in the same direction as long as there are any requests in that direction
+	while (1) {//999 means fault +1,  111 means resolved , 555 meanss termination go back to 0
 		local_command_mutex->Wait();
 		dummy_command = local_command;
 		local_command_mutex->Signal();
 		if (dummy_command == 555) {
-			while(current_floor>0) {
+			while (current_floor > 0) {
 				Sleep(1000);
 				current_floor--;
 				e1.update_status(status, current_floor, direction, door); //updating current status
 			}
+			//open the door at 0 floor
 			if (current_floor == 0) {
-				Sleep(500); //wait
+				Sleep(500); //wait for door to open
 				door = 1;
+				direction = 0;
 				e1.update_status(status, current_floor, direction, door); //updating current status
 			}
 		}
+
 		else {
 			local_command_mutex->Wait();
 			dummy_command = local_command;
@@ -113,6 +120,7 @@ int main(void) {
 			if (dummy_command == 999) {
 				status = 0;
 				e1.update_status(status, current_floor, direction, door); //updating current status
+				//clear all pending requests
 				for (int i = 0; i < 10; i++) {
 					command1_mutex->Wait();
 					floors_to_stop[i] = 0; //clear request
@@ -133,67 +141,60 @@ int main(void) {
 			}
 		}
 
-
-
-
 		//main code
 		local_command_mutex->Wait();
 		dummy_command = local_command;
 		local_command_mutex->Signal();
 
-		if(status==1&& dummy_command!=555){
-		command1_mutex->Wait();
-		dummy = floors_to_stop[current_floor];
-		command1_mutex->Signal();
-		if (dummy) { //stopping at requested stop
-			//stop and open the door, wait for it to close 
-			e1.update_status(status, current_floor, direction, door); //updating current status
-			door = 1; //open door
-			//protecting the array with mutex
+		if (status == 1 && dummy_command != 555) {
 			command1_mutex->Wait();
-			floors_to_stop[current_floor] = 0; //clear request
+			dummy = floors_to_stop[current_floor];
 			command1_mutex->Signal();
+			//stopping at requested stop //stop and open the door, wait for it to close 
+			if (dummy) {
 
-			e1.update_status(status, current_floor, direction, door); //updating current status
+				door = 1; //open door
+				e1.update_status(status, current_floor, direction, door); //updating current status
 
-			Sleep(1000); //wait
+				//protecting the array with mutex //clear floor request
+				command1_mutex->Wait();
+				floors_to_stop[current_floor] = 0; 
+				command1_mutex->Signal();
 
-			//////////////////////////////////////////////////////////////////////////////////////////////
-			//closing door this request will go where we have to decide which direction it's going to move 
-			door = 2;
-			e1.update_status(status, current_floor, direction, door); //updating current status
+				//close the door and update the status
+				Sleep(500); //wait for door close
+				door = 2;
+				e1.update_status(status, current_floor, direction, door); //updating current status
 
-			if (!pending_floor(current_floor, direction)) {
-				direction = getting_direction(current_floor);
+				//DECIDE where to go next: change direction if no more pending requests in the same direction
+				if (!pending_request(current_floor, direction)) {
+					direction = get_direction(current_floor);
+					e1.update_status(status, current_floor, direction, door); //updating current status
+				}
+			}
+
+			//move the lift, change floors
+			if (direction == 0) {
+				direction = get_direction(current_floor);
 				e1.update_status(status, current_floor, direction, door); //updating current status
 			}
-			
-		}
 
-		else { //moving in the directed direction
 			if (direction == 1) {
 				Sleep(1000);
 				current_floor++;
 				e1.update_status(status, current_floor, direction, door); //updating current status
 			}
-			else if (direction == 2) {
+
+			if (direction == 2) {
 				Sleep(1000);
 				current_floor--;
 				e1.update_status(status, current_floor, direction, door); //updating current status
 			}
-			else {
 
-				direction = getting_direction(current_floor);
-				e1.update_status(status, current_floor, direction, door); //updating current status
-
-			}
 		}
 
+	}//while loop end
 
-
-		}//while loop
-
-		t1.WaitForThread();
-
-		return 0;
-	}
+	t1.WaitForThread();
+	return 0;
+}
